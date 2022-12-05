0638f07d8dc5d3e1e707e303b33fef9133ac872b
Bruno Aguiar <evolutionsyndicate@gmail.com>
2022-12-05 17:35:29 -0500

    Continued work on routes for auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,11 @@ class SessionsController < ApplicationController
         user = User.find_by(username: params[:username])
         session[:user_id] = user.id
         render json: user
-      end
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+      
 end
