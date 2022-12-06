7473fdb2a853ef7ca49b8c6018ad1a445cb8af97
Bruno Aguiar <evolutionsyndicate@gmail.com>
2022-12-06 16:04:11 -0500

    Refactored Routes code and create tailwind icon components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,60 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import Signup from './components/Signup'
 import Home from './components/Home'
 import Login from './components/Login';
-import {useState, useEffect} from 'react'
-import Navbar from './components/Navbar';
-
+import { useState, useEffect } from 'react'
 
 function App() {
-const currentUser = sessionStorage.getItem("user_id")
-const [postData, setPostData] = useState([])
+  const currentUser = sessionStorage.getItem("user_id")
+  const [postData, setPostData] = useState([])
 
-useEffect(() => {
-
-
-  if(sessionStorage.getItem("user_id")){
-    fetch("/posts")
-  }
-  
+  useEffect(() => {
 
 
-}, [currentUser])
+    if (sessionStorage.getItem("user_id")) {
+      fetch("/posts")
+    }
 
 
-// useEffect(() => {
-//   fetch("/me").then((resp) => {
-//     if(resp.ok){
-//       resp.json().then((user) => setUser(user))
-//     }
-//   })
-// }, [])
+
+  }, [currentUser])
+
+
+  // useEffect(() => {
+  //   fetch("/me").then((resp) => {
+  //     if(resp.ok){
+  //       resp.json().then((user) => setUser(user))
+  //     }
+  //   })
+  // }, [])
 
 
 
 
   return (
     <>
-      <Router>
-        <Navbar/>
-        <Routes>
+      <Routes>
 
-        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/" element={<Home />} />
 
-        {/* <Route exact path="/" element={
-        user ? (<Home/>) : (<Login replace to={"/login"} handleLogin={handleLogin}/>)}/>    */}
+        <Route path="/login" element={<Login />} />
 
-        <Route path="/login" element={<Login/>}/>
-          
-        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/signup" element={<Signup />} />
 
-        </Routes>
-
-    </Router>
-
-   </>
+      </Routes>
+    </>
   );
 }
 
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {useNavigate} from "react-router-dom";
+import { MdLogin } from "react-icons/md";
 
 function Home() {
+
+  const navigate = useNavigate();
+
+  function login() {
+    navigate('/login');
+  }
+
   return (
-    <div>Home</div>
+    <>
+    <div className='flex'>
+      <div onClick={login} className="flex-1">
+        <PageIcon icon={<MdLogin size="140"/>}/>
+      </div>
+    </div>
+    </>
   )
 }
+
+const PageIcon = ({ icon }) => <div className='page-icon'>{icon}</div>
 
 export default Home
