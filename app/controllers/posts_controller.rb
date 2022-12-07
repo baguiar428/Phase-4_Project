25b79220a958827d9608418ba6249e2a548fb81c
Salman Mahmud <salman_mahmud@hotmail.com>
2022-12-07 00:00:51 -0500

    created resources for posts. Made controller for posts along with all crud actions

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,38 @@
+class PostsController < ApplicationController
+    before_action :find_post
+    skip_before_action :index
+
+    def index 
+        render json: Post.all, status: :ok
+    end
+
+    def show
+        render json: @post, status: :ok
+    end
+
+    def create
+        post = Post.create!(post_params)
+        render json: post, status: :created
+    end
+
+    def update
+        @post.update!(post_params)
+        render json: @post, status: :accepted
+    end
+
+    def destroy
+        @post.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_post
+        @post = Post.find(params[:id])
+    end
+
+    def post_params
+        params.permit(:description, :flair_id)
+    end
+
+end
