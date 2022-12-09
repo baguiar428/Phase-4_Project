80e1fc0522b12e9b2f13e46e46921cce6f54384c
Bruno Aguiar <evolutionsyndicate@gmail.com>
2022-12-09 14:47:13 -0500

    Start of user search component

--- client/src/components/UserSearch.jsx
+++ client/src/components/UserSearch.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function UserSearch({postData, setPostData}) {
+    const currentUser = parseInt(sessionStorage.getItem("user_id"))
+
     return (
+
+        //Backend working via: /search/${username}
+        //
         <></>
     )
 }
